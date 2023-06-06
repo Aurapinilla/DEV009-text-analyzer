1493ab99baf0c75af5dd95504bbb3067184329c8
Aura Pinilla <aura.pinilla12@gmail.com>
2023-06-06 12:52:31 -0500

    commit 1 con las primeras estructuras en código html, css e inicios de javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const soloPalabras = document.getElementById("palabras").value;
+
+function contarPalabras (){
+    palabras;
+    return console.log(palabras.addEventListener("keyup", contarPalabras));
+} 
+
+
+/*
+const buttonEL = document.querySelector("#reset-button");
+
+function borrarTexto(){
+    document.getElementById("areatexto").value = "";
+}
+
+buttonEL.addEventListener('click', borrarTexto);
+*/
