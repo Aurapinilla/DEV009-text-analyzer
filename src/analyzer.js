40500ef0d6de5920598f617343ce271a14f8efa5
Aura Pinilla <aura.pinilla12@gmail.com>
2023-06-06 23:06:58 -0500

    Commit 2 con las primeras funciones del analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,35 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let texto  = document.getElementById("areatexto").value;
+    texto = texto.replace(/\r?\n/g," ");
+    texto = texto.replace(/[ ]+/g," ");
+    texto = texto.replace(/^ /,"");
+    texto = texto.replace(/ $/,"");
+    return((texto).split(" ")).length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return (document.getElementById("areatexto").value).length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let texto  = document.getElementById("areatexto").value;
+    texto = texto.replace( /\s/g, '' );
+    return texto.length; 
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let texto  = document.getElementById("areatexto").value;
+    texto = texto.split(' ').filter(String).join('').split('');
+    return (texto.filter(num => num >= 0 && num <= 9)).length;  
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-
-const soloPalabras = document.getElementById("palabras").value;
-
-function contarPalabras (){
-    palabras;
-    return console.log(palabras.addEventListener("keyup", contarPalabras));
-} 
+const campoTexto = document.querySelector("#areatexto");
 
 
-/*
-const buttonEL = document.querySelector("#reset-button");
+function textAnalyzer(){
+    const texto = document.getElementById("areatexto").innerHTML;
+    const contarPalabras = analyzer.getWordCount(texto);
+    const contarCaracteres = analyzer.getCharacterCount(texto);
+    const contarCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(texto);
+    const contarNumeros = analyzer.getNumberCount(texto);
+    const sumarNumeros = analyzer.getNumberSum(texto);
 
-function borrarTexto(){
+    document.getElementById("resultado1").innerHTML = contarPalabras;
+    document.getElementById("resultado2").innerHTML = contarCaracteres;
+    document.getElementById("resultado3").innerHTML = contarCaracteresSinEspacios;
+    document.getElementById("resultado4").innerHTML = contarNumeros;
+    document.getElementById("resultado5").innerHTML = sumarNumeros;
+}
+console.log(campoTexto.addEventListener("keyup", textAnalyzer));
+
+
+const resetBtn = document.querySelector("#reset-button");
+
+function borrarMetricas(){
     document.getElementById("areatexto").value = "";
+    document.getElementById("resultado1").innerHTML = "0";
+    document.getElementById("resultado2").innerHTML = "0";
+    document.getElementById("resultado3").innerHTML = "0";
+    document.getElementById("resultado4").innerHTML = "0";
 }
 
-buttonEL.addEventListener('click', borrarTexto);
-*/
+resetBtn.addEventListener('click', borrarMetricas);
+
