bb91b59cb4f4990c520541130f933468aaa16be6
Aura <aura.pinilla12@gmail.com>
2023-06-13 12:07:26 -0500

    correcciones test html

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-const textArea = document.querySelector("#areatexto");
+const textArea = document.querySelector('textarea[name = "user-input"]');
+const lists = document.querySelectorAll('li');
 
 function textAnalyzer()
 {
-  const text = document.getElementById("areatexto").value;
+  const text = document.querySelector('textarea[name = "user-input"]').value;
   if ( text !== "")
   {
-    const countWords = analyzer.getWordCount(text);
-    document.getElementById("resultado1").innerHTML = countWords;
+    const countWords = "Palabras: " + analyzer.getWordCount(text);
+    lists[0].innerHTML = countWords;
 
-    const countCharacters = analyzer.getCharacterCount(text);
-    document.getElementById("resultado2").innerHTML = countCharacters;
+    const countCharacters = "Caracteres: " + analyzer.getCharacterCount(text);
+    lists[1].innerHTML = countCharacters;
 
-    const charactersNoSpaces = analyzer.getCharacterCountExcludingSpaces(text);
-    document.getElementById("resultado3").innerHTML = charactersNoSpaces;
+    const charactersNoSpaces = "Caracteres Sin Espacios: " + analyzer.getCharacterCountExcludingSpaces(text);
+    lists[2].innerHTML = charactersNoSpaces;
 
-    const countNumbers = analyzer.getNumberCount(text);
-    document.getElementById("resultado4").innerHTML = countNumbers;
+    const countNumbers = "Recuento Números: " + analyzer.getNumberCount(text);
+    lists[3].innerHTML = countNumbers;
 
-    const sumNumbers = analyzer.getNumberSum(text);
-    document.getElementById("resultado5").innerHTML = sumNumbers;
+    const sumNumbers = "Suma Números: " + analyzer.getNumberSum(text);
+    lists[4].innerHTML = sumNumbers;
 
-    const averageLength = analyzer.getAverageWordLength(text);
-    document.getElementById("resultado6").innerHTML = averageLength;
+    const averageLength = "Longitud Media Palabras: " + analyzer.getAverageWordLength(text);
+    lists[5].innerHTML = averageLength;
   } 
   else 
   {
@@ -38,13 +39,13 @@ const resetBtn = document.querySelector("#reset-button");
 
 function resetMetrics()
 {
-  document.getElementById("areatexto").value = "";
-  document.getElementById("resultado1").innerHTML = "0";
-  document.getElementById("resultado2").innerHTML = "0";
-  document.getElementById("resultado3").innerHTML = "0";
-  document.getElementById("resultado4").innerHTML = "0";
-  document.getElementById("resultado5").innerHTML = "0";
-  document.getElementById("resultado6").innerHTML = "0";
+  document.querySelector('textarea[name = "user-input"]').value = "";
+  lists[0].innerHTML = "Palabras: 0";
+  lists[1].innerHTML = "Caracteres: 0";
+  lists[2].innerHTML = "Caracteres Sin Espacios: 0";
+  lists[3].innerHTML = "Recuento Números: 0";
+  lists[4].innerHTML = "Suma Números: 0";
+  lists[5].innerHTML = "Longitud Media Palabras: 0";
 }
 
 resetBtn.addEventListener('click', resetMetrics);
